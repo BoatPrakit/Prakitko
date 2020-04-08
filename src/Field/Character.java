43d26017caeedde9b5c9f730b3a00432099ebbf0
Boatnb <boatnakrub_@hotmail.com>
2020-04-08 23:33:34 +0700

    [Fix]Bug

--- src/Field/Character.java
+++ src/Field/Character.java
@@ -1,13 +1,32 @@
 package Field;
 
+import status.STATUS;
+
 /**
  *
  * @author User
  */
 public abstract class Character {
+    private final int MAXLEVEL = 30;
     private String name;
+    private int currentHp;
+    private int maxHp;
+    private int atk;
+    private int speed;
+    private int currentStamina;
+    private int level = 1;
+    private STATUS status = STATUS.ALIVE;
+    private double hpPerLvl;
+    private double atkPerLvl;
+    private double staminaPerLvl;
+    private double speedPerLvl;
+    
     protected Character(String name){
         this.name = name;
         
+    }
+    protected Character(String name,int maxHp,int atk,int speed){
+    
+    }
     }
 }
--- src/prototype/Monster.java
+++ src/prototype/Monster.java
@@ -9,10 +9,6 @@ package prototype;
  *
  * @author User
  */
-<<<<<<< HEAD:src/prototype/Monster.java
 public abstract class Monster {
-=======
-public abstract class Item {
->>>>>>> 72086cd364cf7560cfa2f9fdc61e433bece291f9:src/Item/Item.java
     
 }
