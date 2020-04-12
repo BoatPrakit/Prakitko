07493cbce5e711c80160a741e85e005f9f7d03bb
BoatPrakit <boatnakrub_@hotmail.com>
2020-04-13 00:24:48 +0700

    [Fix]Stat

--- src/prakitkomodel/Bird.java
+++ src/prakitkomodel/Bird.java
@@ -8,6 +8,11 @@ import prototype.Prakitko;
  */
 public class Bird extends Prakitko{
     public Bird(String name){
-        super(name,90,35,22,5,4,8,2);
+        //(String name,int maxHp,int atk,int atkSpeed,int maxStamina,int hpPerLvl,int atkPerLvl,int staminaPerLvl,int atkSpeedPerLvl)
+        super(name,90,35,22,100,5,4,8,2);
+    }
+    @Override
+    public String toString(){
+        return "Hp : " + super.getMaxHp() + " Stamina : "+super.getMaxStamina() + " Atk : "+super.getAtk() + " AtkSpeed : "+super.getAtkSpeed();
     }
 }
--- src/prakitkomodel/Fish.java
+++ src/prakitkomodel/Fish.java
@@ -8,10 +8,10 @@ import prototype.Prakitko;
  */
 public class Fish extends Prakitko{
     public Fish(String name){
-        super(name,100,30,24,5,3,20,2);
+        super(name,90,32,21,150,5,3,20,2);
     }
     @Override
     public String toString(){
-        return "";
+        return "Hp : " + super.getMaxHp() + " Stamina : "+super.getMaxStamina() + " Atk : "+super.getAtk() + " AtkSpeed : "+super.getAtkSpeed();
     }
 }
