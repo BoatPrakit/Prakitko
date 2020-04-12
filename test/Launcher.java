e34fb04ca6c413c23d0ac4ea57a91250c15ef4f1
pattarapol <pattarapol.plasma@mail.kmutt.ac.th>
2020-04-12 23:55:55 +0700

    Auto stash before merge of "master" and "origin/master"

--- test/Launcher.java
+++ test/Launcher.java
@@ -1,17 +1,28 @@
 import Field.Character;
+<<<<<<< Updated upstream
 import java.util.ArrayList;
 import prakitkomodel.Cat;
 import prakitkomodel.Dog;
 import prototype.Prakitko;
 
+=======
+import Field.Item;
+import prakitkomodel.Dog;
+import prototype.Prakitko;
+import Item.*;
+>>>>>>> Stashed changes
 public class Launcher {
     public static void main(String[] args) {
-       Prakitko c = new Dog("Test");
-       c.receiveExp(4000);
-        System.out.println(c.getLevel());
-        System.out.println(c.getCurrentExp());
-        System.out.println(c.getCurrentMaxExp());
-
+//       Prakitko c = new Dog("Test");
+//       c.receiveExp(4000);
+//        System.out.println(c.getLevel());
+//        System.out.println(c.getCurrentExp());
+//        System.out.println(c.getCurrentMaxExp());
+//       Item item0 = new Food("Chicken");
+//        System.out.println(item0);
+        
+//       Item item1 = new Medical("First-aid");
+//        System.out.println(item1.regenHp());
         
 }
 }
