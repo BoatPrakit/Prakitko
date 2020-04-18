7d07d8446603d4bc47f659b04278349db00eabaa
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2020-04-18 18:37:28 +0700

    [ADD]Monster

--- src/prototype/Monster.java
+++ src/prototype/Monster.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prototype;
 
-/**
- *
- * @author User
- */
-public abstract class Monster {
+import Field.Item;
+import Field.Character;
+
+public abstract class Monster extends Character{
     
+    private int giveExp;
+    private Item item[];
+    private static int lvl;
+    private int baseExpGive;
+    
+    protected Monster(String name,int maxHp,int atk,int atkSpeed,int maxStamina,int hpPerLvl,int atkPerLvl,int staminaPerLvl,int atkSpeedPerLvl,int lvlRange,int baseExpGive){
+        super(name,randomLvl(lvlRange),calculateMaxHP(maxHp,hpPerLvl),calculateAtk(atk,atkPerLvl),calculateAtkSpeed(atkSpeed,atkSpeedPerLvl),calculateMaxStamina(maxStamina,staminaPerLvl));
+        this.baseExpGive = baseExpGive;
+    }
+   
+    private static int randomLvl(int lvlRange){
+        lvl = (int)Math.round(Math.random()*lvlRange);
+        return lvl;
+    }
+    
+    private static int calculateMaxHP(int maxHp,int hpPerLvl){
+        int result = maxHp+(hpPerLvl*lvl);
+        return result;
+    }
+    private static int calculateAtk(int atk,int atkPerLvl){
+        int result = atk+(atkPerLvl*lvl);
+        return result;
+    }
+    private static int calculateAtkSpeed(int atkSpeed,int atkSpeedPerLvl){
+        int result = atkSpeed+(atkSpeedPerLvl*lvl);
+        return result;
+    }
+    private static int calculateMaxStamina(int maxStamina,int staminaPerLvl){
+        int result = maxStamina+(staminaPerLvl*lvl);
+        return result;
+    }
+    
+    private int dropExp(){
+        int result = (int)(baseExpGive+((Math.random()+1)*lvl));
+        return result;
+    }
+    
+    private Item itemDrop(){
+        return item[0];
+    }
 }
