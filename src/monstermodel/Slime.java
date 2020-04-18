92ef8c5ae46318f8ba72a321d43d46973ff892bd
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2020-04-18 18:00:59 +0700

    [ADD] First Monster Slime

---
+++ src/monstermodel/Slime.java
@@ -0,0 +1,18 @@
+package monstermodel;
+
+import prototype.Monster;
+
+public class Slime extends Monster{
+    private static final String name = "Slime";
+    private static final int baseHp = 50;
+    private static final int baseAtk = 15;
+    private static final int baseSpd = 15;
+    private static final int baseSta = 60;
+    private static final int hpPerlvl = 3;
+    private static final int atkPerlvl = 2;
+    private static final int spdPerlvl = 1;
+    private static final int staPerlvl = 5;
+    public Slime(){       
+        super(name,baseHp,baseAtk,baseSpd,baseSta,hpPerlvl,atkPerlvl,spdPerlvl,staPerlvl);
+    }
+}
