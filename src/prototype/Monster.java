615ea12d7f087e99b95fbaf91a6952700ab47470
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2020-04-28 00:33:45 +0700

    [UPDATE] แก้โครงสร้าง Monster

--- src/prototype/Monster.java
+++ src/prototype/Monster.java
@@ -8,41 +8,22 @@ public abstract class Monster extends Character{
     private int giveExp;
     private Item item[];
     private static int lvl;
-    private int baseExpGive;
     
     protected Monster(String name,int maxHp,int atk,int atkSpeed,int maxStamina,int hpPerLvl,int atkPerLvl,int staminaPerLvl,int atkSpeedPerLvl,int lvlRange,int baseExpGive){
-        super(name,randomLvl(lvlRange),calculateMaxHP(maxHp,hpPerLvl),calculateAtk(atk,atkPerLvl),calculateAtkSpeed(atkSpeed,atkSpeedPerLvl),calculateMaxStamina(maxStamina,staminaPerLvl));
-        this.baseExpGive = baseExpGive;
-    }
-   
-    private static int randomLvl(int lvlRange){
-        lvl = (int)Math.round(Math.random()*lvlRange);
-        return lvl;
-    }
-    
-    private static int calculateMaxHP(int maxHp,int hpPerLvl){
-        int result = maxHp+(hpPerLvl*lvl);
-        return result;
-    }
-    private static int calculateAtk(int atk,int atkPerLvl){
-        int result = atk+(atkPerLvl*lvl);
-        return result;
-    }
-    private static int calculateAtkSpeed(int atkSpeed,int atkSpeedPerLvl){
-        int result = atkSpeed+(atkSpeedPerLvl*lvl);
-        return result;
-    }
-    private static int calculateMaxStamina(int maxStamina,int staminaPerLvl){
-        int result = maxStamina+(staminaPerLvl*lvl);
-        return result;
-    }
-    
-    private int dropExp(){
-        int result = (int)(baseExpGive+((Math.random()+1)*lvl));
-        return result;
+        super(name,lvlRange,maxHp,atk,atkSpeed,maxStamina,hpPerLvl,atkPerLvl,atkSpeedPerLvl,staminaPerLvl,baseExpGive);
     }
     
     private Item itemDrop(){
         return item[0];
     }
+    
+    private void addItem(Item item){
+        
+    }
+    
+    @Override
+    public String toString(){
+        return getName()+"\n Level : "+getLevel()+"\n HP : "+getMaxHp()+ "\n ATK : "+getAtk();
+    }
+    
 }
