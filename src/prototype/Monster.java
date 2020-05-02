0ff0419f552c31ba5cc00c9e0e75e1ea8432c30e
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2020-05-02 14:20:26 +0700

    [UPDATE] Monster class have item

--- src/prototype/Monster.java
+++ src/prototype/Monster.java
@@ -2,24 +2,22 @@ package prototype;
 
 import Field.Item;
 import Field.Character;
+import java.util.ArrayList;
+import java.util.Random;
 
 public abstract class Monster extends Character{
     
-    private Item item[];
+    private ArrayList<Item> item = new ArrayList();
     private static int lvl;
+    private Random randomGenerator = new Random();
     
-    protected Monster(String name,int maxHp,int atk,int atkSpeed,int maxStamina,int hpPerLvl,int atkPerLvl,int staminaPerLvl,int atkSpeedPerLvl,int lvlRange,int baseExpGive){
+    protected Monster(String name,int maxHp,int atk,int atkSpeed,int maxStamina,int hpPerLvl,int atkPerLvl,int staminaPerLvl,int atkSpeedPerLvl,int lvlRange,int baseExpGive,ArrayList<Item> item){
         super(name,lvlRange,maxHp,atk,atkSpeed,maxStamina,hpPerLvl,atkPerLvl,atkSpeedPerLvl,staminaPerLvl,baseExpGive);
+        this.item = item;
     }
     
     public Item itemDrop(){
-        return item[0];
-    }
-    
-    private void addItem(Item item){
-        
-    }
-   
-    
-    
+        int index = randomGenerator.nextInt(item.size());
+        return item.get(index);
+    } 
 }
