dc34a28822b15ba9c552d6e47844cbfd6208dd0a
BoatPrakit <boatnakrub_@hotmail.com>
2020-05-02 22:39:07 +0700

    For diz

--- src/Model/Item.java
+++ src/Model/Item.java
@@ -12,15 +12,6 @@ public class Item {
     private int regenStamina;
     private ITEMTYPE itemtype;
     
-    public Item (){
-        this.regenHp = regenHp;
-        this.id = id;
-        this.name = name;
-    }
-    public Item(int id,String name){
-        this.id = id;
-        this.name = name;
-    }
     public int getId() {
         return id;
     }
@@ -71,7 +62,7 @@ public class Item {
         this.id = id;
     }
     
-    public void setRegenHp(int rengenHp){
+    public void setRegenHp(int regenHp){
         this.regenHp = regenHp;
     }
     
